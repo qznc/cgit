15553db5fe662cc123b796a442a711d45ee2e200
Andreas Zwinkau <zwinkau@kit.edu>
2011-02-25 14:11:04 +0100

    More atom feed tests for the diffs

--- tests/t0109-atom.sh
+++ tests/t0109-atom.sh
@@ -6,15 +6,20 @@ prepare_tests "Check content on atom feed page"
 
 run_test 'fetch feed' 'cgit_url "foo/atom" >trash/tmp'
 
-
-run_test 'find feed open tag' '
-	grep -e "^<feed xmlns=.http://www\.w3\.org/2005/Atom.>" trash/tmp
-'
+run_test 'find feed open tag' 'grep -e "^<feed xmlns=.http://www\.w3\.org/2005/Atom.>" trash/tmp'
 
 run_test 'find title' 'grep -e "^<title>foo" trash/tmp'
 
 run_test 'find entry title' 'grep -e "^<title>commit 5</title>" trash/tmp'
 
-run_test 'find entry id' 'grep -e "^<id>406d4fbf364415255f2493a0e78e2a7f0d5db85b</id>" trash/tmp'
+run_test 'find entry diffstat' 'grep -e "^<div class=.diffstat-header.>" trash/tmp'
+
+run_test 'find entry diff' 'grep -e "<table summary=.diff. class=.diff.>" trash/tmp'
+
+run_test 'find diff content 1' 'grep -e "diff --git a/file-1 b/file-1" trash/tmp'
+
+run_test 'find diff content 2' 'grep -e "@@ -0,0 +1 @@" trash/tmp'
+
+run_test 'find diff content 3' 'grep -e "+1<" trash/tmp'
 
 tests_done
